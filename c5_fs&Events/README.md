098521db98c4acb1debebd2a0919fb3fbf282d2e
Monu Tomar <monusinghtomar107@gmail.com>
2025-04-23 14:08:15 +0530

    c4

---
+++ c5_fs&Events/README.md
@@ -0,0 +1,1 @@
+All about file sysytem and events 
---
+++ c5_fs&Events/src/app.js
@@ -0,0 +1,26 @@
+// console.log("All about files and events");
+// 1. File System
+const fs = require("fs");
+// const fs = require("fs").promises; // for async/await
+
+const res = fs.readFileSync("./src/data.txt", "utf-8");
+console.log(res);
+
+// const res2 = fs.readFile("./src/data.txt", "utf-8", (err, data) => {
+//     if (err) {
+//         console.log(err);
+//         return;
+//     }
+//     console.log(data);
+// });
+// console.log(res2); // undefined because readFile is async and returns immediately
+
+
+
+
+// const path = require("path");
+// const { readFile, writeFile } = require("fs").promises;
+// const { readFileSync, writeFileSync } = require("fs");
+// const { readFile, writeFile } = require("fs").promises;
+// const { readFileSync, writeFileSync } = require("fs");
+
